8c2bd9185d12f18cfc92ed809cc87cadbc41abf4
Emiliano Santos <santos@MacBook-Air-de-Emiliano-6.local>
2025-11-02 00:32:41 -0600

    Se implementó la posibiliad de guardar las respuestas previas del examen, así, se muestra pregunta por pregunta, teniendo un registro de las respuestas que ya había seleccionado un usuario.

--- Frontend/script/app.js
+++ Frontend/script/app.js
@@ -6,6 +6,14 @@
 
 import servicios from "./servicios.js";
 
+let preguntas;
+let respuestasUsuario = {};
+
+window.guardarRespuesta = (preguntaId, valorSeleccionado) => {
+  respuestasUsuario[preguntaId] = valorSeleccionado;
+  console.log("Respuestas Actuales:", respuestasUsuario);
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   servicios.actualizarSesion();
 
@@ -18,35 +26,62 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 
   document.getElementById("btnPreguntas").onclick = async () => {
-    const preguntas = await servicios.start();
+    preguntas = await servicios.start();
 
     if (preguntas) {
       localStorage.setItem("question", 0);
-      cargarPreguntas(preguntas);
+      cargarPregunta();
     }
   };
 
-  const cargarPreguntas = (preguntas) => {
+  const cargarPregunta = () => {
     const pregunta = document.getElementById("pregunta");
     pregunta.innerHTML = "";
 
     const p = preguntas[localStorage.getItem("question")];
+
+    const respuestaGuardada = respuestasUsuario[p.id];
 
     const div = document.createElement("div");
     div.className = "card";
     div.innerHTML = `
                   <p><strong>${p.id}.</strong> ${p.text}</p>
                   ${p.options
-                    .map(
-                      (opt) => `
-                    <label>
-                      <input type="radio" name="q_${p.id}" value="${opt}"> ${opt}
+                    .map((opt) => {
+                      const isChecked =
+                        respuestaGuardada === opt ? "checked" : "";
+
+                      return `
+                        <label>
+                      <input type="radio" name="q_${p.id}" value="${opt}" onchange="guardarRespuesta(${p.id}, this.value)" ${isChecked}> ${opt}
                     </label><br>
-                  `
-                    )
+                  `;
+                    })
                     .join("")}
                 `;
     pregunta.appendChild(div);
     document.getElementById("quizForm").style.display = "block";
+  };
+
+  document.getElementById("btnSiguiente").onclick = () => {
+    const indice = parseInt(localStorage.getItem("question"), 10);
+
+    if (indice === 7) {
+      return;
+    } else {
+      localStorage.setItem("question", indice + 1);
+    }
+    cargarPregunta();
+  };
+
+  document.getElementById("btnAnterior").onclick = () => {
+    const indice = parseInt(localStorage.getItem("question"), 10);
+
+    if (indice === 0) {
+      return;
+    } else {
+      localStorage.setItem("question", indice - 1);
+    }
+    cargarPregunta();
   };
 });
