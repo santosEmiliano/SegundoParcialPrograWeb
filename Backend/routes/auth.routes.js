b0d6de0d632bff9098755fb1ce820e4380b2a3f9
Emiliano Santos <santos@MacBook-Air-de-Emiliano-6.local>
2025-11-02 18:30:31 -0600

    Solución de algunos errores

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -32,4 +32,10 @@ router.get("/lectura", verifyToken, sendPublications)
 
 //          RUTAS DE VERIFICACION
 
+//Ruta de verificacion de compra
+router.post("/compra", verifyToken, checkBuy);
+
+//Ruta de verificacion de examen realizado
+router.post("/pasado", verifyToken, checkExam);
+
 module.exports = router;
